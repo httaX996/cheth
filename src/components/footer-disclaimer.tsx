a401c1bc7d8fe1a67bda8f82f9cead7c08b338c1
httaX996 <damindakule@gmail.com>
2025-09-04 14:22:19 +0530

    Update footer-disclaimer.tsx

--- src/components/footer-disclaimer.tsx
+++ src/components/footer-disclaimer.tsx
@@ -87,8 +87,10 @@ const FooterDisclaimer: React.FC = () => {
                                 mt: { xs: 4, md: 0, lg: 0 },
                                 opacity: .6
                             }}>
-                                Ez Streaming @ {new Date().getFullYear()} <br style={{ marginBottom: 5 }} />
-                                All media content available through this application is sourced from publicly available internet streams. We do not own, host, or distribute any of the content. All content remains the property of their respective owners
+                                © CK CineMAX {new Date().getFullYear()} <br style={{ marginBottom: 5 }} />
+                                All media content available through this application is sourced from publicly available internet streams. We do not own, host, or distribute any of the content. All content remains the property of their respective owners.
+
+                                Developer: Chethmina Kavishan
                             </Typography>
 
                             <Typography sx={{
